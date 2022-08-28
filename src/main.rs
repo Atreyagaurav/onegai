daf2b19758269b81a5f5cb2dddd6b5a9e09d66fd
Gaurav <allmanpride@gmail.com>
2022-08-28 18:54:22 -0400

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -62,21 +62,13 @@ fn main() {
 }
 
 fn make_html(ignore_match_err: bool, input_files: Vec<PathBuf>) {
-    let readers: Vec<Lines<BufReader<File>>> = input_files
-        .iter()
-        .map(|fp| {
-            let file = File::open(fp.clone()).expect(&format!(
-                "Couldn't open input file {}.",
-                fp.as_os_str().to_str().unwrap()
-            ));
-            let lines = BufReader::new(file).lines();
-            return lines;
-        })
-        .collect();
     let lines: Vec<usize> = input_files
         .iter()
         .map(|f| {
-            let file = File::open(f).unwrap();
+            let file = File::open(f.clone()).expect(&format!(
+                "Couldn't open input file {}.",
+                f.as_os_str().to_str().unwrap()
+            ));
             BufReader::new(file).lines().count()
         })
         .collect();
@@ -86,24 +78,35 @@ fn make_html(ignore_match_err: bool, input_files: Vec<PathBuf>) {
         }
     }
     let num_lines: usize = *lines.iter().min().unwrap();
+
+    let readers: Vec<Lines<BufReader<File>>> = input_files
+        .iter()
+        .map(|fp| {
+            let file = File::open(fp.clone()).unwrap();
+            let lines = BufReader::new(file).lines();
+            return lines;
+        })
+        .collect();
     println!("{}", get_html(num_lines, readers).into_string());
 }
 
 fn get_html(num_lines: usize, mut readers: Vec<Lines<BufReader<File>>>) -> Markup {
     html! {
     (DOCTYPE)
-    h3 {"Title"}
-        @for _ in 0..num_lines {
-        div id="line"{
-        @for reader in readers.iter_mut() {
-            // reader.read_line(&mut line).unwrap();
+        h3 {"Title"}
+        @for i in 1..=num_lines {
+            @let linetag=format!("line-{}",i);
+            div id=(linetag) {
+        p {
+            @for reader in readers.iter_mut() {
             @match reader.next() {
-                Some(line) => p {
-            (line.unwrap())},
+                Some(line) => {(line.unwrap())},
                 None => {""},
             }
+        br;
+            }
         }}
-    }}
+        }}
 }
 
 fn replace_from_json(replacement_json: PathBuf, input_file: PathBuf, output_file: PathBuf) {
@@ -111,15 +114,15 @@ fn replace_from_json(replacement_json: PathBuf, input_file: PathBuf, output_file
 }
 
 fn translate(input_file: PathBuf, _output_file: PathBuf) {
+    let file = File::open(input_file).expect("Couldn't open input file.");
+    let reader = BufReader::new(file);
+
     let model = TranslationModelBuilder::new()
         .with_model_type(ModelType::MBart)
         .with_source_languages(vec![Language::Japanese])
         .with_target_languages(vec![Language::English])
         .create_model()
         .unwrap();
-
-    let file = File::open(input_file).expect("Couldn't open input file.");
-    let reader = BufReader::new(file);
 
     let mut line: String;
     for input_line in reader.lines() {
