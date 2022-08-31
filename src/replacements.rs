87fb38befd0549d56da2126022325b4910be07a9
Gaurav-Lab-PC <atreyagv@mail.uc.edu>
2022-08-31 18:02:10 -0400

    Finalize the algorithm for replacing patterns
    * Tested with previous python script the results are similar.
    * unordered replacement may have caused some differences

--- src/replacements.rs
+++ src/replacements.rs
@@ -25,14 +25,11 @@ struct Replacements {
 }
 
 pub fn replace_from_json(replacement_json: PathBuf, input_file: PathBuf, output_file: PathBuf) {
-    println!("{:?} {:?} {:?}", replacement_json, input_file, output_file,);
     let replace_json_contents =
         fs::read_to_string(replacement_json).expect("Cannot read Replacement File.");
     let rep_cont: Replacements =
         serde_json::from_str(&replace_json_contents).expect("Can't parse Json");
     verify_rules(&rep_cont);
-    print_rules(&rep_cont);
-    println!("{:?}", rep_cont.honorifics);
 
     let input_file_contents = fs::read_to_string(input_file).expect("Cannot read input file.");
     let output_file_contents = replace_string(&rep_cont, input_file_contents);
@@ -44,17 +41,6 @@ fn verify_rules(rep: &Replacements) {
         rep.contents
             .get(rule.key.clone())
             .expect(&format!("No Key: {} found in replacements json", rule.key));
-    }
-}
-
-fn print_rules(rep: &Replacements) {
-    for rule in rep.rules.iter() {
-        println!(
-            "{} [{}]: {}",
-            rule.name,
-            rule.key,
-            rule.description.as_ref().unwrap_or(&String::from(""))
-        );
     }
 }
 
@@ -80,9 +66,7 @@ fn replace_string(rep: &Replacements, contents: String) -> String {
                 let rep_dict: HashMap<String, Vec<String>> =
                     serde_json::from_str(&rep.contents.get(&rule.key).unwrap().to_string())
                         .expect(&format!("Cannot parse contents of key: {}.", &rule.key));
-                let names_dict =
-                    generate_names_hasmap(rep_dict, &rep.honorifics, first_name, last_name);
-                output = replace_hashmap(output, &names_dict);
+                output = replace_names(output, &rep_dict, &rep.honorifics, first_name, last_name);
             } else {
                 let (first_name, _) = rule.honorifics.unwrap();
                 let rep_dict: HashMap<String, String> =
@@ -108,15 +92,26 @@ fn replace_string(rep: &Replacements, contents: String) -> String {
     return output;
 }
 
-fn generate_names_hasmap(
-    rep_dict: HashMap<String, Vec<String>>,
+fn replace_names(
+    contents: String,
+    rep_dict: &HashMap<String, Vec<String>>,
     honorifics: &HashMap<String, String>,
     first_name: bool,
     last_name: bool,
-) -> HashMap<String, String> {
-    let mut names_dict = HashMap::<String, String>::new();
+) -> String {
+    let mut output: String = contents;
     for (en_name, jp_names) in rep_dict.iter() {
-        names_dict.insert(jp_names.join(""), en_name.to_string());
+        for (hon_jp, hon_en) in honorifics.iter() {
+            output = replace_single(
+                output,
+                &format!("{}{}", jp_names.join(""), hon_jp),
+                &format!("{}-{}", en_name, hon_en),
+            );
+        }
+        output = replace_single(output, &jp_names.join("・"), &en_name.to_string());
+        output = replace_single(output, &jp_names.join(""), &en_name.to_string());
+    }
+    for (en_name, jp_names) in rep_dict.iter() {
         let en_names: Vec<&str> = en_name.split(" ").collect();
         assert!(
             en_names.len() == jp_names.len(),
@@ -124,29 +119,49 @@ fn generate_names_hasmap(
         );
         for i in 0..en_names.len() {
             for (hon_jp, hon_en) in honorifics.iter() {
-                names_dict.insert(
-                    format!("{}{}", jp_names[i], hon_jp),
-                    format!("{}-{}", en_names[i], hon_en),
+                output = replace_single(
+                    output,
+                    &format!("{}{}", jp_names[i], hon_jp),
+                    &format!("{}-{}", en_names[i], hon_en),
                 );
-                if i == 0 && !first_name {
-                    names_dict.insert(format!("{}", jp_names[i]), format!("{}", en_names[i]));
-                } else if i == en_names.len() - 1 && !last_name {
-                    names_dict.insert(format!("{}", jp_names[i]), format!("{}", en_names[i]));
-                }
+            }
+            // len > 3 to filter names with single kanji (len is 3 for
+            // one kanji), so that they won't be replaced in random
+            // words.  Those names will only be replaced when they
+            // occur with honorifics. Very hard to tell it's a name
+            // from the code otherwise.
+            if i == 0 && !first_name && jp_names[i].len() > 3 {
+                output = replace_single(
+                    output,
+                    &format!("{}", jp_names[i]),
+                    &format!("{}", en_names[i]),
+                );
+            } else if i == en_names.len() - 1 && !last_name && jp_names[i].len() > 3 {
+                output = replace_single(
+                    output,
+                    &format!("{}", jp_names[i]),
+                    &format!("{}", en_names[i]),
+                );
             }
         }
     }
-    return names_dict;
+    return output;
 }
 
 fn replace_hashmap(contents: String, map: &HashMap<String, String>) -> String {
     let mut output: String = contents;
     for (find, replace) in map.iter() {
-        let count = output.matches(find).count();
-        if count > 0 {
-            println!("{} → {} ({})", find, replace, count);
-            output = output.replace(find, &replace);
-        }
+        output = replace_single(output, find, replace);
+    }
+    return output;
+}
+
+fn replace_single(contents: String, find: &String, replace: &String) -> String {
+    let mut output: String = contents;
+    let count = output.matches(find).count();
+    if count > 0 {
+        output = output.replace(find, &replace);
+        println!("{} [{}]→ {} ({})", find, find.len(), replace, count);
     }
     return output;
 }
